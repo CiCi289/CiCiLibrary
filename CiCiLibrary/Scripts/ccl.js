cff698eeb761dec470780d2d5170a129ebef279a
CiCi289 <cihtinaung289.cha@gmail.com>
2023-05-21 11:16:30 +0630

    extra comments made in JQUERY code

--- CiCiLibrary/Scripts/ccl.js
+++ CiCiLibrary/Scripts/ccl.js
@@ -1,36 +1,35 @@
-﻿$(function () {         //when document is ready...
+﻿$(function () {                                             //when document is ready...
     // Creating new book
-    $('#createBook').on('click', function () {          //when this element is clicked, do the following
-        $.get('/Book/Create', function (data) {
-            $('#modalContent').html(data);
-            $('#modal').modal('show');
+    $('#createBook').on('click', function () {              //when this element is clicked, do the following
+        $.get('/Book/Create', function (data) {             //make GET req to create url, get data, 
+            $('#modalContent').html(data);                  //insert into modalContent element
+            $('#modal').modal('show');                      //show up modal
         });
     });
 
     // Editing a book
-    $(document).on('click', '.editBook', function () {
-        var id = $(this).data('id');
-        $.get('/Book/Edit/' + id, function (data) {
-            $('#modalContent').html(data);
-            $('#modal').modal('show');
+    $(document).on('click', '.editBook', function () {      //when this button is clicked, do the following
+        var id = $(this).data('id');                        //get data-id's value aka book Id 
+        $.get('/Book/Edit/' + id, function (data) {         //make GET req to edit+id url, get data,
+            $('#modalContent').html(data);                  //insert into modalContent element
+            $('#modal').modal('show');                      //show up modal
         });
     });
 
     // Saving/Submitting a book
-    $(document).on('click', '#saveBook', function () {
-        var form = $('#bookForm');
-        $.ajax({
-            url: form.attr('action'),
+    $(document).on('click', '#saveBook', function () {      //when this button is clicked, do the following
+        var form = $('#bookForm');                          //select element with id of bookForm
+        $.ajax({                                            //make AJAX form POST request 
+            url: form.attr('action'),                       //according to action, method, and serialize data
             type: form.attr('method'),
             data: form.serialize(),
-            success: function (response) {
-                
-                if (response.success) {
+            success: function (response) {                  //if success property is received from response
+                if (response.success) {                     
                     console.log(response);
-                    $('#modal').modal('hide');
+                    $('#modal').modal('hide');              //modal gets hidden
                     //location.reload(); // Refresh page
                 } else {
-                    $('#modalContent').html(response);
+                    $('#modalContent').html(response);      //else, response data is added into modal 
                 }
                
             }
@@ -38,13 +37,13 @@
     });
 
     // Deleting a book
-    $(document).on('click', '.deleteBook', function () {
-        var id = $(this).data('id');
-        if (confirm('Are you sure you want to delete this book?')) {
-            $.post('/Book/Delete/' + id, function (response) {
-                if (response.success) {
+    $(document).on('click', '.deleteBook', function () {    //when this button is clicked, do the following
+        var id = $(this).data('id');                        //select element with id of book 
+        if (confirm('Are you sure you want to delete this book?')) {//show confirm deletion modal 
+            $.post('/Book/Delete/' + id, function (response) {      //make AJAX POST request to delete+id url, get response
+                if (response.success) {                             //if success
                     alert('Deleted successfully!');
-                    updateBookList();
+                    updateBookList();                               //update page (bookList) with fresh data , w/o reload
                     //location.reload(); // Refresh page
                 }
             });
@@ -73,9 +72,9 @@
 
      //to update the bookList div
     function updateBookList() {
-        $.get('/Book/Index', function (data) {
-            var bookListContent = $(data).find('#bookList').html();
-            $('#bookList').html(bookListContent);
-        });
+        $.get('/Book/Index', function (data) {                          //make GET req to Index url, get data
+            var bookListContent = $(data).find('#bookList').html();     //select element with id of bookList from data $() is surrounded for Jquery funcs
+            $('#bookList').html(bookListContent);                       //insert into bookList with selected data element
+        });     
     }
 });
