a26712ff4cbfefa4b51f7de276af3ef549aee9dc
CiCi289 <cihtinaung289.cha@gmail.com>
2023-05-22 12:14:41 +0630

    Not much changes

--- CiCiLibrary/Scripts/ccl.js
+++ CiCiLibrary/Scripts/ccl.js
@@ -9,7 +9,7 @@
 
     // Editing a book
     $(document).on('click', '.editBook', function () {      //when this button is clicked, do the following
-        var id = $(this).data('id');                        //get data-id's value aka book Id 
+        let id = $(this).data('id');                        //get data-id's value aka book Id 
         $.get('/Book/Edit/' + id, function (data) {         //make GET req to edit+id url, get data,
             $('#modalContent').html(data);                  //insert into modalContent element
             $('#modal').modal('show');                      //show up modal
@@ -18,7 +18,7 @@
 
     // Saving/Submitting a book
     $(document).on('click', '#saveBook', function () {      //when this button is clicked, do the following
-        var form = $('#bookForm');                          //select element with id of bookForm
+        let form = $('#bookForm');                          //select element with id of bookForm
         $.ajax({                                            //make AJAX form POST request 
             url: form.attr('action'),                       //according to action, method, and serialize data
             type: form.attr('method'),
@@ -38,7 +38,7 @@
 
     // Deleting a book
     $(document).on('click', '.deleteBook', function () {    //when this button is clicked, do the following
-        var id = $(this).data('id');                        //select element with id of book 
+        let id = $(this).data('id');                        //select element with id of book 
         if (confirm('Are you sure you want to delete this book?')) {//show confirm deletion modal 
             $.post('/Book/Delete/' + id, function (response) {      //make AJAX POST request to delete+id url, get response
                 if (response.success) {                             //if success
@@ -73,7 +73,7 @@
      //to update the bookList div
     function updateBookList() {
         $.get('/Book/Index', function (data) {                          //make GET req to Index url, get data
-            var bookListContent = $(data).find('#bookList').html();     //select element with id of bookList from data $() is surrounded for Jquery funcs
+            let bookListContent = $(data).find('#bookList').html();     //select element with id of bookList from data $() is wrapped for Jquery funcs
             $('#bookList').html(bookListContent);                       //insert into bookList with selected data element
         });     
     }
